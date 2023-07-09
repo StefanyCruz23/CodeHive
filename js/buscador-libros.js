abcf022ca94ad8a72c461ae69f3d527a10ff340c
StefanyCruz23 <134335214+StefanyCruz23@users.noreply.github.com>
2023-07-09 10:40:37 -0600

    Update buscador-libros.js
    Agregar la funcion master de SetTime

--- js/buscador-libros.js
+++ js/buscador-libros.js
@@ -2,51 +2,59 @@ const searchForm = document.getElementById("searchForm");
 const searchInput = document.getElementById("searchInput");
 const bookContainer = document.getElementById("bookContainer");
 
-// Función para realizar la búsqueda y mostrar los resultados
-const searchBooks = (event) => {
-  event.preventDefault(); // Evita el envío del formulario por defecto
 
-  const searchTerm = searchInput.value.trim(); // Obtiene el término de búsqueda
+const searchBooks = (event) => {
+  event.preventDefault(); 
+
+  const searchTerm = searchInput.value.trim(); 
   if (searchTerm === "") {
-    return; // Si no hay término de búsqueda, no se realiza nada
+    return; 
   }
 
-  const url = `https://openlibrary.org/search.json?title=${encodeURIComponent(searchTerm)}`;
+  
+  const messageElement = document.createElement("div");
+  messageElement.textContent = "Buscando el libro, espere...";
+  bookContainer.appendChild(messageElement);
 
-  // Utilizamos la función fetch para obtener los datos de la API
-  fetch(url)
-    .then(response => response.json()) // Convertimos la respuesta a JSON
-    .then(data => {
-      bookContainer.innerHTML = ""; // Limpiamos el contenedor de libros
+  
+  setTimeout(() => {
+    const url = `https://openlibrary.org/search.json?title=${encodeURIComponent(searchTerm)}`;
 
-      // Recorremos los resultados y creamos elementos HTML para cada libro
-      data.docs.slice(0, 5).forEach(book => {
-        const bookElement = document.createElement("div");
-        bookElement.classList.add("book");
-        bookElement.innerHTML = `
-          <h3>${book.title}</h3>
-          <p>Autor: ${book.author_name ? book.author_name.join(", ") : "Desconocido"}</p>
-          <p>Año de publicación: ${book.first_publish_year || "Desconocido"}</p>
-          <button>Ver más</button>
-          <hr>
-        `;
+    
+    fetch(url)
+      .then(response => response.json()) 
+      .then(data => {
+        bookContainer.innerHTML = ""; 
 
-        // Agregamos el evento de clic para redirigir al libro
-        bookElement.addEventListener("click", () => {
-          window.location.href = `https://openlibrary.org${book.key}`;
+        
+        data.docs.slice(0, 5).forEach(book => {
+          const bookElement = document.createElement("div");
+          bookElement.classList.add("book");
+          bookElement.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>Autor: ${book.author_name ? book.author_name.join(", ") : "Desconocido"}</p>
+            <p>Año de publicación: ${book.first_publish_year || "Desconocido"}</p>
+            <button>Ver más</button>
+            <hr>
+          `;
+
+          
+          bookElement.addEventListener("click", () => {
+            window.location.href = `https://openlibrary.org${book.key}`;
+          });
+
+          
+          bookContainer.appendChild(bookElement);
         });
-
-        // Agregamos el elemento del libro al contenedor
-        bookContainer.appendChild(bookElement);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      })
+      .finally(() => {
+        searchInput.value = ""; 
       });
-    })
-    .catch(error => {
-      console.error("Error:", error);
-    })
-    .finally(() => {
-      searchInput.value = ""; // Limpiamos el campo de búsqueda después de la búsqueda
-    });
+  }, 3000); 
 };
 
-// Escuchamos el evento de envío del formulario
+
 searchForm.addEventListener("submit", searchBooks);
